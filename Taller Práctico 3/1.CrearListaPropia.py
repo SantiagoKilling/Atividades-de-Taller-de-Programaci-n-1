b70c8ed6cb3c0c5861d77e046beff7e54a9c9db3
SantiagoKilling <santiagokilling@gmail.com>
2026-07-30 17:47:55 -0300

    Creando el primer ejercicio del Taller Práctico 3

---
+++ Taller Práctico 3/1.CrearListaPropia.py
@@ -0,0 +1,10 @@
+"""
+Crea tu propia lista: Crea una lista llamada hobbies con al menos 5 de tus
+pasatiempos. Imprime la lista completa. Luego, imprime solo el primer y el último
+hobby usando índices (hobbies[0] y hobbies[-1]).
+"""
+
+ListaHobbies = ["Tocar guitarra", "Hacer Ejercicio", "Componer música", "Jugar videojuegos", "Leer libros"]
+print("Lista completa de hobbies:", ListaHobbies)
+print("Primer hobby:", ListaHobbies[0])
+print("Último hobby:", ListaHobbies[-1])
